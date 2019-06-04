e9a8c4508a43e7d2c0d288339fdf82499e54a287
partySquirrel <putsepp@gmail.com>
2019-06-04 10:16:44 +0300

    highlighting winning row

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
+  const className = 'square' + (props.winningItem ? ' winning' : '');
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick} className={className}>
       {props.value}
     </button>
   );
@@ -26,6 +27,7 @@ class Board extends React.Component {
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        winningItem={this.props.winningLine.includes(i)}
       />
     );
   }
@@ -83,7 +85,7 @@ class Game extends React.Component {
   }
 
   isGameOver(squares, i) {
-    return calculateWinner(squares) || squares[i] || isTie(this.state.stepNumber);
+    return calculateWinner(squares).winner || squares[i] || isTie(this.state.stepNumber);
   }
 
   jumpTo(step) {
@@ -99,7 +101,8 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
 
-    let status = this.getStatus(current);
+    const result = calculateWinner(current.squares);
+    let status = this.getStatus(result);
 
     const moves = history.map((step, move) => {
       const desc = move ? 'Go to move # ' + move : 'Go to game start';
@@ -118,6 +121,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            winningLine={result.winningLine}
           />
         </div>
         <div className="game-info">
@@ -128,10 +132,9 @@ class Game extends React.Component {
     );
   }
 
-  getStatus(current) {
-    const winner = calculateWinner(current.squares);
-    if (winner) {
-      return 'Winner is: ' + winner;
+  getStatus(result) {
+    if (result.winner) {
+      return 'Winner is: ' + result.winner;
     } else if (isTie(this.state.stepNumber)) {
       return 'It\'s a draw!';
     }
@@ -158,10 +161,16 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return ({
+        winner: squares[a],
+        winningLine: lines[i],
+      });
     }
   }
-  return null;
+  return ({
+    winner: null,
+    winningLine: [],
+  });
 }
 
 
