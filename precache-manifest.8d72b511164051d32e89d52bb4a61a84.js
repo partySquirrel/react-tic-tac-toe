0a6b6402443ba21e1a57a6ab55d10c02be85517a
partySquirrel <putsepp@gmail.com>
2019-06-05 16:44:44 +0300

    Updates

---
+++ precache-manifest.8d72b511164051d32e89d52bb4a61a84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346c9c3d24b0ec8aac722a61f035cbf8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "31028c8de24a8a3a04f5",
+    "url": "/my-app/static/css/main.f9838123.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/my-app/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "31028c8de24a8a3a04f5",
+    "url": "/my-app/static/js/main.e5c91cb0.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
